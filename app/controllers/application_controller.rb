07e09bed11764610999b96905a077b7b3e244222
Koy Saeteurn <kosatun@aol.com>
2018-10-05 06:10:08 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,14 +16,7 @@ class ApplicationController < Sinatra::Base
     erb :signup
   end
 
-  post "/signup" do
-    if params[:username] == "" || params[:password] == ""
-      redirect "/failure"
-    else 
-      User.create(username: params[:username], password: params[:password])
-      redirect "/login"
-    end   
-  end
+  
 
   get '/account' do
     @user = User.find(session[:user_id])
